e9fdfd439f9ece999cc372836e8486f3551c4563
joekgilberto <joekgilberto@gmail.com>
2024-01-18 18:09:53 -0500

    builds out ValidateRecordId validator

--- service/Models/Hurricane.cs
+++ service/Models/Hurricane.cs
@@ -1,34 +1,41 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace service.Models
 {
 	public class Hurricane
 	{
+        [Required]
         [StringLength(2, MinimumLength = 2, ErrorMessage = "Basin must be two characters long.")]
         public string Basin
 		{ get; set; }
 
-        [StringLength(2, MinimumLength = 2, ErrorMessage = "ATCF code must be two characters long.")]
-        public string Atcf
+        [Required]
+        [Range(1, 99)]
+        public int AtcfNumber
         { get; set; }
 
+        [Required]
         [Range(1851, 2022)]
         public int Year
 		{ get; set; }
 
+        [Required]
         public string Name
         { get; set; }
 
+        [Required]
         public int TrackEntryCount
         { get; set; }
 
+        [Required]
         public List<Object> TrackEntries
         { get; set; }
 
-        public Hurricane(string basin, string atcf, int year, string name, int trackEntryCount, List<Object> trackEntries)
+        public Hurricane(string basin, int atcfNumber, int year, string name, int trackEntryCount, List<Object> trackEntries)
 		{
 			Basin = basin;
-			Atcf = atcf;
+            AtcfNumber = atcfNumber;
 			Year = year;
             Name = name;
             TrackEntryCount = trackEntryCount;
--- service/Models/TrackEntry.cs
+++ service/Models/TrackEntry.cs
@@ -1,11 +1,51 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using service.CustomValidation;
+
 namespace service.Models
 {
 	public class TrackEntry
 	{
-		public TrackEntry()
+        [Required]
+        [Range(1851, 2022)]
+        public int Year
+        { get; set; }
+
+        [Required]
+        [Range(1, 12)]
+        public int Month
+        { get; set; }
+
+        [Required]
+        [Range(1, 31)]
+        public int Day
+        { get; set; }
+
+        [Required]
+        [Range(0, 24)]
+        public int Hours
+        { get; set; }
+
+        [Required]
+        [Range(0, 59)]
+        public int Minutes
+        { get; set; }
+
+        [Required]
+        [ValidateRecordId]
+        [StringLength(1, MinimumLength = 1, ErrorMessage = "RecordId must be one characters long.")]
+        public string RecordId
+        { get; set; }
+
+        public TrackEntry(int year, int month, int day, int hours, int minutes, string recordId)
 		{
-		}
+			Year = year;
+            Month = month;
+            Day = day;
+            Hours = hours;
+            Minutes = minutes;
+            RecordId = recordId;
+        }
 	}
 }
 
