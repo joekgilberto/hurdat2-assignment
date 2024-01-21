6a2e9a7c205e646079d0dbf73df192c9592789d5
joekgilberto <joekgilberto@gmail.com>
2024-01-21 05:07:34 -0500

    removes extra link in footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 export default function Footer() {
   return (
     <footer>
-      <p>This <a href='https://github.com/joekgilberto/hurdat2-assignment' target='_blank' rel='noreferrer'>HURDAT2 assignment</a> is made with <a href='https://dotnet.microsoft.com/en-us/apps/aspnet' target='_blank' rel='noreferrer'>ASP.NET</a>, <a href='https://react.dev/' target='_blank' rel='noreferrer'>React</a>, <a href='https://redux.js.org/' target='_blank' rel='noreferrer'>Redux</a> <a href='https://mui.com/material-ui/' target='_blank' rel='noreferrer'>Material UI</a>, HURDAT2 data from  <a href='https://www.nhc.noaa.gov/data' target='_blank' rel='noreferrer'>the National Hurricane Center</a>, and Florida GEOJSON data from <a href='https://github.com/danielcs88/fl_geo_json/tree/master' target='_blank' rel='noreferrer'>danielcs88 on GitHub</a> by <a href='https://joekgilberto.com/' target='_blank' rel='noreferrer'>Joe Gilberto</a>.</p>
+      <p>This <a href='https://github.com/joekgilberto/hurdat2-assignment' target='_blank' rel='noreferrer'>HURDAT2 assignment</a> is made with <a href='https://dotnet.microsoft.com/en-us/apps/aspnet' target='_blank' rel='noreferrer'>ASP.NET</a>, <a href='https://react.dev/' target='_blank' rel='noreferrer'>React</a>, <a href='https://redux.js.org/' target='_blank' rel='noreferrer'>Redux</a>, HURDAT2 data from  <a href='https://www.nhc.noaa.gov/data' target='_blank' rel='noreferrer'>the National Hurricane Center</a>, and Florida GEOJSON data from <a href='https://github.com/danielcs88/fl_geo_json/tree/master' target='_blank' rel='noreferrer'>danielcs88 on GitHub</a> by <a href='https://joekgilberto.com/' target='_blank' rel='noreferrer'>Joe Gilberto</a>.</p>
     </footer>
   );
 }
