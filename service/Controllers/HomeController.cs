ebc57f714059b9626555d74c13c013c6686ff866
joekgilberto <joekgilberto@gmail.com>
2024-01-22 16:31:46 -0500

    adds controller for / route

---
+++ service/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace service.Controllers
+{
+    [ApiController]
+    //Creates an empty landing route
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        //Creates a GetHome controller that returns a string
+        [HttpGet("")]
+        public string GetHome()
+        {
+            //returns the string "HURDAT2 parsed by Joe Gilberto"
+            return "HURDAT2 parsed by Joe Gilberto";
+        }
+    }
+}
+
--- service/Controllers/HurricaneController.cs
+++ service/Controllers/HurricaneController.cs
@@ -1,65 +1,65 @@
-﻿using service;
+﻿using System;
 using Microsoft.AspNetCore.Mvc;
 using service.Models;
 using service.Data;
 
-namespace service.Controllers;
-
-[ApiController]
-//Creates a route of "huricane"
-[Route("[controller]")]
-public class HurricaneController : ControllerBase
+namespace service.Controllers
 {
-    //Creates a property of _context to hold an instance of HurricaneData
-    private readonly HurricaneData _context;
-
-    //Creates a constructor for HurricaneController
-    public HurricaneController()
+    [ApiController]
+    //Creates a route of "huricane"
+    [Route("[controller]")]
+    public class HurricaneController : ControllerBase
     {
-        //Initiates an instance of HurricaneData and assigns it to the property _context, working as a data source for the application
-        HurricaneData context = new HurricaneData();
-        _context = context;
-    }
+        //Creates a property of _context to hold an instance of HurricaneData
+        private readonly HurricaneData _context;
 
-    //Creates a GetAll controller that returns all hurricanes
-    [HttpGet("all")]
-    public List<Hurricane> GetAll()
-    {
-        //Creates a hurricanes variable that calls all Hurricanes stored in _context.Hurricanes (an instance of HurricaneData)
-        List<Hurricane> hurricanes = _context.Hurricanes;
+        //Creates a constructor for HurricaneController
+        public HurricaneController()
+        {
+            //Initiates an instance of HurricaneData and assigns it to the property _context, working as a data source for the application
+            HurricaneData context = new HurricaneData();
+            _context = context;
+        }
 
-        //Returns said hurricanes variable
-        return hurricanes;
-    }
+        //Creates a GetAll controller that returns all hurricanes
+        [HttpGet("all")]
+        public List<Hurricane> GetAll()
+        {
+            //Creates a hurricanes variable that calls all Hurricanes stored in _context.Hurricanes (an instance of HurricaneData)
+            List<Hurricane> hurricanes = _context.Hurricanes;
 
-    //Creates a GetByATCFCode controller that returns a specific hurricane called by an ATCF Code, accepting a string labeled ATCFCode
-    [HttpGet("{ATCFCode}")]
-    public List<Hurricane> GetByATCFCode(string ATCFCode)
-    {
-        //Returns a list of hurricanes where the hurricane's ATCF Code equeals the inputed string, which only includes one matching hurricane
-        List<Hurricane> hurricane = _context.Hurricanes.Where(h => h.ATCFCode == ATCFCode).ToList();
+            //Returns said hurricanes variable
+            return hurricanes;
+        }
 
-        //Returns the found hurricane in list form (or an empty list if no hurricane is found)
-        return hurricane;
-    }
+        //Creates a GetByATCFCode controller that returns a specific hurricane called by an ATCF Code, accepting a string labeled ATCFCode
+        [HttpGet("{ATCFCode}")]
+        public List<Hurricane> GetByATCFCode(string ATCFCode)
+        {
+            //Returns a list of hurricanes where the hurricane's ATCF Code equeals the inputed string, which only includes one matching hurricane
+            List<Hurricane> hurricane = _context.Hurricanes.Where(h => h.ATCFCode == ATCFCode).ToList();
 
-    //Creates a GetLandfalls controller that returns all landfalls
-    [HttpGet("landfalls")]
-    public List<Landfall> GetLandfalls()
-    {
-        //Assigns a List of Landfalls to the List of Landfalls kepy in the instance of HurricaneData
-        List<Landfall> landfalls = _context.Landfalls;
+            //Returns the found hurricane in list form (or an empty list if no hurricane is found)
+            return hurricane;
+        }
 
-        //Returns the List of Landfalls
-        return landfalls;
-    }
+        //Creates a GetLandfalls controller that returns all landfalls
+        [HttpGet("landfalls")]
+        public List<Landfall> GetLandfalls()
+        {
+            //Assigns a List of Landfalls to the List of Landfalls kepy in the instance of HurricaneData
+            List<Landfall> landfalls = _context.Landfalls;
 
-    //Creates a GetTest test controller that returns the string "test" to help developers ensure the application is running
-    [HttpGet("test")]
-    public string GetTest()
-    {
-        //Returns a string, "test"
-        return "test";
+            //Returns the List of Landfalls
+            return landfalls;
+        }
+
+        //Creates a GetTest test controller that returns the string "test" to help developers ensure the application is running
+        [HttpGet("test")]
+        public string GetTest()
+        {
+            //Returns a string, "test"
+            return "test";
+        }
     }
 }
-
